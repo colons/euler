d46cb9f32482f4948abae3c196dee4900151c434
Iain Dawson <iain@bldm.us>
2012-04-29 10:34:15 +0100

    Modified factorial().

--- common.py
+++ common.py
@@ -7,16 +7,17 @@ def factors(n):
         if n % d == 0:
             f = n/d
             fs.append(d)
+
             if d == sqrt and f not in fs:
                 fs.append(f)
             elif d != sqrt:
                 fs.append(f)
 
-
-
         d += 1
 
     return fs
+
+
 
 def is_prime(number):
     if number == 1:
@@ -30,9 +31,8 @@ def is_prime(number):
 
 def factorial(n):
     f = 1
-    while n != 0:
+    while n > 0:
         f *= n
         n -= 1
 
     return f
-
