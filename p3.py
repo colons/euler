3857065e9f850ca595e73615db99658dbff3cea7
Iain Dawson <iain@bldm.us>
2012-04-29 11:19:15 +0100

    Improved 3.

--- p3.py
+++ p3.py
@@ -5,10 +5,7 @@ big_number = 600851475143
 sqr = int(big_number ** 0.5)
 f = sqr
 
-while True:
-    if f == 1:
-        break
-
+while f != 1:
     if big_number % f == 0 and is_prime(f):
         print f
         break
