069c24ff95bb72c8baf25246045ee13e4b119219
Iain Dawson <iain@colons.co>
2014-05-18 16:05:28 +0100

    Implement a brute force solution to problem 15 that would take too long.

--- p15.py
+++ p15.py
@@ -1,5 +1,28 @@
-xw = 2
-yw = 2
+import re
 
-for i in xrange(xw):
-    
+
+size = 20
+
+
+def is_route(route):
+    bin_string = bin(route)[2:]
+    while len(bin_string) < size*2:
+        bin_string = '0' + bin_string
+
+    return len(re.findall('1', bin_string)) == size
+
+
+route = 0
+possible_route_count = (2**size)**2
+routes = []
+
+while route < possible_route_count:
+    if route % 1000 == 0:
+        print '{doing}/{of}'.format(doing=route, of=possible_route_count)
+
+    if is_route(route):
+        routes.append(route)
+
+    route += 1
+
+print len(routes)
